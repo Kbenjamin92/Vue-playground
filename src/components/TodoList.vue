a905519e9dca28627aad7e9dec8ce39cf4dc78a2
Kbenjamin92 <kipplbenjamin@gmail.com>
2025-01-28 15:05:48 -0500

    add styles to todolist

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -8,15 +8,30 @@ const props = defineProps({
 
 <template>
   <div>
-    <ul v-for="item in items">
+    <ul id="list-container" v-for="item in items">
       <li class="list-items">{{ item.title }}</li>
       <li class="list-items">{{ item.description }}</li>
+      <button class="delete-button">delete</button>
     </ul>
   </div>
 </template>
 
 <style scoped>
+#list-container {
+  border: 1px solid #d3d3d3;
+  border-radius: 0.5rem;
+}
 .list-items {
   list-style: none;
+}
+
+.delete-button {
+  border: 1px solid crimson;
+  border-radius: 0.2rem;
+  background-color: crimson;
+  color: aliceblue;
+  padding: 0.5rem;
+  font-size: 14px;
+  font-weight: bold;
 }
 </style>
